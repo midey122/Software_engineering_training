105fca16e08ff71857463f232960a03b3b9729d5
midey122 <olajideayomide946@gmail.com>
2023-09-11 06:35:59 -0700

    Day1

---
+++ Dy1/first.py
@@ -0,0 +1,6 @@
+x = "what is computer" #string
+y = "24" #string
+number,list,tuple = 32 ,[45],(76) #integer
+list = ["123",23,23.45] #list
+tuple = ("1333",232,[1234,345])
+print(type(tuple))
---
+++ Dy1/fourth.py
@@ -0,0 +1,3 @@
+mylist = [34,"std1",34,"this is a string ",34,345.54,[12,"this is a list",[ "12",34,"this is an xample of a list"]],[12,"this is a list",[ "12",34,"this is an xample of a list"]]]
+mylist.append("this is the last index")
+print(mylist.count([12,"this is a list",[ "12",34,"this is an xample of a list"]]))
